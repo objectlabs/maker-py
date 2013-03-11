a137412bcad749eb87c68e01c9d1ed150b722cc0
abdul <abdul@mongolab.com>
2013-03-11 13:07:31 -0700

    maker 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name='maker-py',
-    version='0.1.0',
+    version='0.1.1',
     author='MongoLab Team',
     author_email='team@mongolab.com',
     description='maker for python',
