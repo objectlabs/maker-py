021ca59d2b648476eb652f440447f8948cc9e8d1
abdul <abdul@mongolab.com>
2013-04-09 16:44:45 -0700

    bumping up pymaker version to 0.1.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name='maker-py',
-    version='0.1.1',
+    version='0.1.2',
     author='MongoLab Team',
     author_email='team@mongolab.com',
     description='maker for python',
