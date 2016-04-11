2080ca43846bb2c9b2b0d70b18a244fad6b282ac
abdul <abdul@objectlabs.com>
2016-04-11 14:18:06 -0700

    update version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name='makerpy',
-    version='0.3.2',
+    version='0.3.3',
     author='MongoLab Team',
     author_email='team@mongolab.com',
     description='maker for python',
